162f0ec23a9d710c17a54134ee79becdab7eff4f
KKISHORE007 <kkishore.tsp@gmail.com>
2026-08-28 21:08:26 +0530

    chore: minor formatting update in inventoryRoutes.js

--- backend/src/routes/inventoryRoutes.js
+++ backend/src/routes/inventoryRoutes.js
@@ -11,3 +11,5 @@ router.route('/').get(protect, getInventory);
 router.route('/movement').post(protect, authorize('Admin', 'Manager', 'Staff'), recordMovement);
 
 module.exports = router;
+
+// style: minor formatting update 34
